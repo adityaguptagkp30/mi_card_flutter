8de8d19bc0c61b1a873eba6dc283beee3f08dc23
Aditya Gupta <adityaguptagkp30@gmail.com>
2020-04-06 08:49:59 +0530

    Added photo inside circular avatar with profile description

--- lib/main.dart
+++ lib/main.dart
@@ -13,45 +13,34 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
             // verticalDirection: VerticalDirection.up,
             //crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.red,
-                //child: Text('Container 1'),
+              CircleAvatar(
+                backgroundImage: AssetImage('images/aditya1.jpg'),
+                radius: 50,
               ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.yellow,
-                //child: Text('Container 1'),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                //color: Colors.green,
-                decoration: BoxDecoration(
-                  //color: Colors.yellow,
-                  border: Border.all(color: Colors.black, width: 8),
-                  gradient: LinearGradient(
-                    colors: const [
-                      Colors.yellow,
-                      Colors.red,
-                    ],
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(28)),
+              Text(
+                'Aditya',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
                 ),
-                // child: Text('Container 1'),
               ),
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.blue,
-                // child: Text('Container 1'),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ],
           ),
